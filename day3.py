b03dd3cc72f9c13377b598ed4eedec6d6ccbc38f
deekshitha N <deekshithadeekshitha2024@gmail.com>
2026-05-17 20:32:42 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -2,13 +2,13 @@ age = input("age: ")#taking input from the user
 print(age)
 
 boy_name=input("boy name>> ")
-boy_age=int(input("boy age>> "))
+boy_age=int(input("boy age>> "))# age should be int
 girl_name=input("girl_name>> ")
-girl_age=int(input("girl age>> "))                                      
+girl_age=int(input("girl age>> "))#age should be int                                      
 age_diff = abs(boy_age-girl_age)#absolute gives only positive value
 print(f"{boy_name} loves {girl_name} and their age difference is {age_diff}")#format string
 
 
 #commets
-"""whdueufjehf"""
-'''dwjhefwnefuyuwejgd37''' #anything between triple quotes the interpreter ignores it
+"""whdueuf nbvgvn  b fdgfc  m ngfcv hgfcv bnkkuyjgfcv bnmjehf"""
+'''dwjhefwnefuyuw mnmnnmbhvghbmnnhjgejgd37''' #anything between triple quotes the interpreter ignores it
