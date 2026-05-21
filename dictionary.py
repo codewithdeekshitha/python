044df7d80e067680063ad6fd46bd04fb5bc350e4
codewithdeekshitha <deekshithadeekshitha2024@gmail.com>
2026-05-21 19:44:52 +0530

    added dictionary program

---
+++ dictionary.py
@@ -0,0 +1,45 @@
+#dictionary
+
+birthday={                   #dictionary
+    "darshan": "24-11-2006",    #key: value
+    "deekshitha": "20-03-2007",
+    "aishu": "17-11-2008"
+}
+meaning={
+    "pen": "used to write",    #key: value
+    "books": "used to read",
+    "chair": "used to sit"
+}
+print(birthday["darshan"]) #accessing dictionary
+print(birthday.get("aishu"))
+print(birthday.get("shwetha","not found")) #for safe accessing
+birthday["prasad"]="20-07-1978" #adding value in run time
+print(birthday)
+birthday["deekshitha"]="20-03-2003" #updating the value
+print(birthday)
+birthday.pop("aishu") #to remove particular data use key
+print(birthday)
+del birthday["deekshitha"] #another way of deleting
+print(birthday)
+
+#dictionary methods
+print(birthday.keys())
+print(birthday.values())
+print(birthday.items())
+
+new_item={"bed": " used to sleep"}
+meaning.update(new_item)
+print(meaning)
+ #list cannot be used in dict but we can use int,string,float,boolean
+ 
+d1={
+     "name": "sugar",
+     "weight": 1,
+     "price": 50
+ }
+d2={
+     "name": "ghee",
+     "weight": 2,
+     "price": 150
+ }
+print(f"total weight:{d1["weight"]+d2["weight"]}")
