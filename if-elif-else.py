ffc67bcd1f532b6af1be7ac0c47e9f00692b1303
deekshitha N <deekshithadeekshitha2024@gmail.com>
2026-05-27 21:51:13 +0530

    Update if-elif-else.py

--- if-elif-else.py
+++ if-elif-else.py
@@ -8,31 +8,31 @@ x=19
 if x%2==0:
     print("x is an even number")
 else:
-    print("x is an odd number")
+    print("x is an odd number") #o/p=x is odd number
     
-signal=input("enter the colour of signal: ")
+signal=input("enter the colour of signal: ") #green
 if signal=="red":
     print("stop")
 elif signal=="yellow":
     print("ready")
 else:
-    print("go")    
+    print("go")    #o/p=go
     
 #logical operators in if statements
 att=75
 is_teacher_friend=True
 if att<=65 or is_teacher_friend:
-    print("exam is given")
+    print("exam is given")   #o/p=exam is given
 else:
     print("no exam is given")    
     
 #checking bus ticket
 
-gender=input("enter your gender>> ")
-age=int(input("enter your age>> "))
+gender=input("enter your gender>> ") #female
+age=int(input("enter your age>> ")) #34
 is_conductor_frnd=True
 if gender=="female":
-    print("ticket is free")
+    print("ticket is free") #o/p=ticket is free
 else:    
     if age>=65 and is_conductor_frnd:
         print("u get a free ticket")
