7e6b7e55a95f0bd53303bca86faac1af895e31a5
codewithdeekshitha <deekshithadeekshitha2024@gmail.com>
2026-05-28 20:45:45 +0530

    Added whileloop program

---
+++ while.py
@@ -0,0 +1,10 @@
+pin="2024"
+trials=1
+while trials<=3:
+    input_pin=input(f"trial-{trials} | pin ")
+    trials+=1
+    if input_pin==pin:
+        print("correct")
+    else:
+        print("incorrect")    
+    
