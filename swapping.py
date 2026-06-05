2dbbea86a366b18b9660fe7a9c6024dda415fdbf
deekshitha N <deekshithadeekshitha2024@gmail.com>
2026-06-05 22:10:30 +0530

    Update swapping.py

--- swapping.py
+++ swapping.py
@@ -5,7 +5,16 @@ b=20
 print("before swapping")
 print("a=",a)
 print("b=",b)
-a,b=b,a
+a,b=b,a #a value is b and b value is a
 print("after swapping")
 print("a=",a)
 print("b=",b)
+
+#o/p:
+'''before swapping 
+a=10
+b=20
+after swapping
+a=20
+b=10
+'''
