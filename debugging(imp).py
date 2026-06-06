b833c7cb309637f2f83ad58f33fc435e14acab36
deekshitha N <deekshithadeekshitha2024@gmail.com>
2026-06-06 22:01:57 +0530

    Update debugging(imp).py

--- debugging(imp).py
+++ debugging(imp).py
@@ -9,3 +9,8 @@
 for i in range(1,3):
     for j in range(1,11):
         print(f"{i}X{j}={i*j}")
+
+#step 1:add a break point
+#step 2:now run and debug the program
+#step  3:now it start tracing the program in local 
+#step 4:in watch it checks the condition
