8bdd7ad37098cfc90900437d5b9033bbe28e2375
deekshitha N <deekshithadeekshitha2024@gmail.com>
2026-06-06 21:59:42 +0530

    Add files via upload

---
+++ debugging(imp).py
@@ -0,0 +1,11 @@
+
+    
+#debug : find the error and remove it
+#debugger : it a tool used for debug and
+#           to understand the flow/execution of a program    
+
+
+
+for i in range(1,3):
+    for j in range(1,11):
+        print(f"{i}X{j}={i*j}")
