b54fa1fd211fd9b094332696d6dccea110d85ac1
deekshitha N <deekshithadeekshitha2024@gmail.com>
2026-06-17 23:57:36 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,9 +1,9 @@
 age = input("age: ")#taking input from the user
 print(age)
 
-boy_name=input("boy name>> ")
+boy_name=input("boy name>> ")#taking input from the user
 boy_age=int(input("boy age>> "))# age should be int
-girl_name=input("girl_name>> ")
+girl_name=input("girl_name>> ")#taking input from the user
 girl_age=int(input("girl age>> "))#age should be int                                      
 age_diff = abs(boy_age-girl_age)#absolute gives only positive value
 print(f"{boy_name} loves {girl_name} and their age difference is {age_diff}")#format string
